4b13cac31c3a410b4136a4938557ef185fc622ae
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-02-20 12:37:49 +0100

    Update manager.py

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -89,7 +89,7 @@ import selfdrive.crash as crash
 from selfdrive.loggerd.config import ROOT
 
 # set logging level
-logging.basicConfig(level=logging.info)
+logging.basicConfig(level=logging.INFO)
 logging.info('So should this')
 logging.warning('And this, too')
 
