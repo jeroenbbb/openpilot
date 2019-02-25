1ae39930e952cdddd7e6911ba902c01f2c4871cf
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-02-25 17:24:52 +0100

    Update config.py

--- selfdrive/loggerd/config.py
+++ selfdrive/loggerd/config.py
@@ -5,5 +5,8 @@ if os.environ.get('LOGGERD_ROOT', False):
   print("Custom loggerd root: ", ROOT)
 else:
   ROOT = '/data/media/0/realdata/'
+  if not os.path.exists(ROOT):
+    # take the params file, that should exist
+    ROOT = '/data/params/d'
 
 SEGMENT_LENGTH = 60
