d8907b8fa4ab6564b5b2d666a13f0f509e2f0d38
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-02-28 09:30:15 +0100

    Update geofence.py

--- selfdrive/sensord/geofence.py
+++ selfdrive/sensord/geofence.py
@@ -1,9 +1,15 @@
 # check geofence vs gps position
 # and send result on zmq
 
-import shapely
+#import shapely
+from shapely.geometry import Polygon
 
 #read geofence from parameter file
+points = [(34.093523, -118.274893), (34.091414, -118.275887), (34.092082, -118.278062), (34.093867, -118.276609), (34.093523, -118.274893)]
+polygon = Polygon(points)
+
+# the area in square degrees, usefull?
+# area_sdeg = polygon.area
 
 # get gps position from zmq
 
