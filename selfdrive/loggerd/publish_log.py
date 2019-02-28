8c72bbbdb27b1e7499b97200735582b4bdda0424
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-02-28 16:43:29 +0100

    Create publish_log.py

---
+++ selfdrive/loggerd/publish_log.py
@@ -0,0 +1,17 @@
+# read all messages from zmq
+# and send them to a website using POST
+# the website may store and publish them
+
+import time
+import zmq
+import selfdrive.messaging as messaging
+
+def listen_to_all():
+    # port = "12344"       
+    context = zmq.Context()
+    thermal_sock = messaging.sub_sock(context, service_list['thermal'].port)
+    msg = messaging.recv_sock(thermal_sock, wait=True)
+    print msg
+
+if __name__ == "__main__":
+    listen_to_all()
