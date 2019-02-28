e179ae209478b182f0b9b26f637cedb41e7bee36
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-02-28 11:12:04 +0100

    Create gps.py

---
+++ selfdrive/sensord/gps.py
@@ -0,0 +1,8 @@
+# read gps data from a usb gps
+# using nmea and gpsd
+# http://doschman.blogspot.com/2013/01/parsing-nmea-sentences-from-gps-with.html
+# https://raspberrytips.nl/ublox-neo-6m-gps-raspberry-pi-3/
+# http://www.danmandle.com/blog/getting-gpsd-to-work-with-python/
+# http://www.catb.org/gpsd/
+# http://www.catb.org/gpsd/hardware.html
+
