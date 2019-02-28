2a594c89e41108003d75c3f1fd1804100676886c
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-02-28 17:41:43 +0100

    Update publish_log.py

--- selfdrive/loggerd/publish_log.py
+++ selfdrive/loggerd/publish_log.py
@@ -40,8 +40,8 @@ def listen_to_all():
                 if isinstance(msg, str):
                     print (service + "=" + msg)
                 else:
-                    print ("message received from " + service + " ")
-                    type(msg)
+                    print ("message received from " + service + " " + str(msg))
+                    #type(msg)
             count = count + 1
         
         time.sleep(1)
