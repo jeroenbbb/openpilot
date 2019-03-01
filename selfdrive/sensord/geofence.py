d17f3237d4c1572be769be57caebbad1247e2f73
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-01 14:23:26 +0100

    Update geofence.py

--- selfdrive/sensord/geofence.py
+++ selfdrive/sensord/geofence.py
@@ -5,7 +5,8 @@
 #import shapely
 from shapely.geometry import Polygon
 
-#read geofence from parameter file
+# read geofence from parameter file
+# bloemendaal = 52.3992479,4.630414
 points = [(34.093523, -118.274893), (34.091414, -118.275887), (34.092082, -118.278062), (34.093867, -118.276609), (34.093523, -118.274893)]
 polygon = Polygon(points)
 
@@ -13,11 +14,21 @@ polygon = Polygon(points)
 # area_sdeg = polygon.area
 
 # get gps position from zmq
+lat = 52.3992479
+lon = 4.630414
+speed = 5
+bearing = 270
 
 # check if position is within the fence
+
 
 # predict next position using bearing and speed
 
 # and check the predicted position
 
 # and send results to zmq
+
+# is separate prcoess because it is slow: display 
+# import reverse_geocoder as rg
+# coordinates = (51.5214588,-0.1729636),(9.936033, 76.259952),(37.38605,-122.08385)
+# results = rg.search(coordinates) # default mode = 2
