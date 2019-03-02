f0c3d9a1a77daa909c3dd7db871eaecaae224ebb
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-02 19:40:05 +0100

    Update gps.py

--- selfdrive/sensord/gps.py
+++ selfdrive/sensord/gps.py
@@ -7,3 +7,11 @@
 # http://www.catb.org/gpsd/hardware.html
 # https://github.com/Knio/pynmea2
 
+import usb
+busses = usb.busses()
+for bus in busses:
+    devices = bus.devices
+    for dev in devices:
+        print "Device:", dev.filename
+        print "  idVendor: %d (0x%04x)" % (dev.idVendor, dev.idVendor)
+        print "  idProduct: %d (0x%04x)" % (dev.idProduct, dev.idProduct)
