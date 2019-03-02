06e6965e45cfe5df8faff02fb4779a7cf0585564
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-02 13:25:37 +0100

    Update thermald.py

--- selfdrive/thermald.py
+++ selfdrive/thermald.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import pip #needed to use the pip functions
+for i in pip.get_installed_distributions(local_only=True):
+    print(i)
+    
 import os
 import zmq
 import psutil
