262b6d55d3699fd9246172196de7774e8a356119
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-02 13:55:31 +0100

    Update thermald.py

--- selfdrive/thermald.py
+++ selfdrive/thermald.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python
-
-import pip #needed to use the pip functions
-for i in pip.get_installed_distributions(local_only=True):
-    print(i)
-    
 import os
 import zmq
 import psutil
@@ -44,8 +39,7 @@ def read_thermal():
   dat.thermal.mem = read_tz(2)
   dat.thermal.gpu = read_tz(16)
   dat.thermal.bat = read_tz(29)
-  dat.thermal.cpu0Percent = 0
-  #dat.thermal.cpu0Percent = psutil.cpu_percent()
+  dat.thermal.cpu0Percent = psutil.cpu_percent()
   
   # for raspberry and single cpu hardware
   if dat.thermal.cpu0 == 0:
