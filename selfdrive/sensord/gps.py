d08acf3482dd61fd8bda0a28571fa11845c8d1d1
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-02 21:02:31 +0100

    Update gps.py

--- selfdrive/sensord/gps.py
+++ selfdrive/sensord/gps.py
@@ -6,10 +6,13 @@
 # http://www.catb.org/gpsd/
 # http://www.catb.org/gpsd/hardware.html
 # https://github.com/Knio/pynmea2
+# http://www.linux-usb.org/usb.ids  lists all vendors, product id in hex
+# https://en.wikipedia.org/wiki/USB#Device_classes   (devclass 9 = hub)
 
 import usb
 busses = usb.busses()
 for bus in busses:
+    print (vars(bus))
     devices = bus.devices
     for dev in devices:
         dev.__dict__.keys()
@@ -18,4 +21,5 @@ for bus in busses:
         print ("  idVendor: %d (0x%04x)" % (dev.idVendor, dev.idVendor))
         print ("  idProduct: %d (0x%04x)" % (dev.idProduct, dev.idProduct))
         print ("  devnum: " + str(dev.iManufacturer) + str(dev.devnum))
+        print ("  dev: " + str(dev.dev))
         
