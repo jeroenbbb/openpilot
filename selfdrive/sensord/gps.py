3b25ef823add2dc65275253178a114ae4646ce6e
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-02 19:41:27 +0100

    Update gps.py

--- selfdrive/sensord/gps.py
+++ selfdrive/sensord/gps.py
@@ -12,6 +12,6 @@ busses = usb.busses()
 for bus in busses:
     devices = bus.devices
     for dev in devices:
-        print "Device:", dev.filename
-        print "  idVendor: %d (0x%04x)" % (dev.idVendor, dev.idVendor)
-        print "  idProduct: %d (0x%04x)" % (dev.idProduct, dev.idProduct)
+        print ("Device:", dev.filename)
+        print ("  idVendor: %d (0x%04x)" % (dev.idVendor, dev.idVendor))
+        print ("  idProduct: %d (0x%04x)" % (dev.idProduct, dev.idProduct))
