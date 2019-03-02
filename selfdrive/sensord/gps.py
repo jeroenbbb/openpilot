7c603f4f544e5e32fb87bce499f3ceee307a6670
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-02 20:25:38 +0100

    Update gps.py

--- selfdrive/sensord/gps.py
+++ selfdrive/sensord/gps.py
@@ -17,11 +17,5 @@ for bus in busses:
         print ("Device:", dev.filename)
         print ("  idVendor: %d (0x%04x)" % (dev.idVendor, dev.idVendor))
         print ("  idProduct: %d (0x%04x)" % (dev.idProduct, dev.idProduct))
-        print ("  iMan: " + str(dev.iManufacturer) + str(dev.bConfigurationValue))
+        print ("  devnum: " + str(dev.iManufacturer) + str(dev.devnum))
         
-
-for dev in usb.core.find(find_all=True):
-    print (dev.get_active_configuration())
-    print ("Device:", dev.filename)
-    print ("  idVendor: %d (%s)" % (dev.idVendor, hex(dev.idVendor)))
-    print ("  idProduct: %d (%s)" % (dev.idProduct, hex(dev.idProduct)))
