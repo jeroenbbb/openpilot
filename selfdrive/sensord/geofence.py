386a44f739bcbcb55b58eccc6c4fb91817752876
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-04 21:32:55 +0100

    Update geofence.py

--- selfdrive/sensord/geofence.py
+++ selfdrive/sensord/geofence.py
@@ -36,7 +36,7 @@ d = Point(52.3992479,4.630414).distance(s2)
 print (d)
 
 d = nearest_points(Point(52.3992479,4.630414), s2)
-print (d)
+print (d.wkt)
 
 # the area in square degrees, usefull?
 # area_sdeg = polygon.area
