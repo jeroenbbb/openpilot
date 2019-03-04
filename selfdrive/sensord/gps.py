deacd892ff678c23c61f1e2552282e858fc9278b
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-04 12:42:45 +0100

    Update gps.py

--- selfdrive/sensord/gps.py
+++ selfdrive/sensord/gps.py
@@ -75,7 +75,7 @@ for new_data in gpsd_socket:
         latitude = data_stream.TPV['lat']
         print('Altitude = ',data_stream.TPV['alt'])
         print('Latitude = ',latitude)
-
+        latitude = float64(5)
     else:
         # noting received
         print ("Nothing" + str(count))
