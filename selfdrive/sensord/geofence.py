cc2bb374bc66d1af61d2d8394e22b3397f64d984
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-05 18:03:37 +0100

    Update geofence.py

--- selfdrive/sensord/geofence.py
+++ selfdrive/sensord/geofence.py
@@ -1,15 +1,16 @@
 # check geofence vs gps position
 # and send result on zmq
-# https://shapely.readthedocs.io/en/latest/manual.html#introduction
-# IsGeofenceEnabled (0/1) and the GeoFence data are stored in params
-# a geofence is stored in geojson format
-# see also https://stackoverflow.com/questions/16942697/geojson-circles-supported-or-not
 #
-# this program reads the geofences from tha param file and the gps location from zmq
+# this program reads the geofences from the param file and the gps location from zmq
+#   IsGeofenceEnabled (0/1) and the GeoFence data are stored in params
+#   a geofence is stored in geojson format
+#   the zmq message = gpsLocationExternal
+
 # it uses shapely to find the nearest point and distance
+#   see https://shapely.readthedocs.io/en/latest/manual.html#introduction
 # if the distance=0, the gps location is in the fence
 #   using the bearing and speed, the next point after 1 second is estimated and again checked against the fence
-# else it uses geopy to calculate the distance between the 2 points
+# it uses geopy to calculate the distance between the 2 points
 # if current-point and future-point are within the fence, geofence = green
 # if current-point is in the fence but future point not, geofence = orange
 # if current-point and future-point are outside the fence, geofence = red
@@ -34,47 +35,48 @@ from shapely import speedups
 if speedups.available:
   speedups.enabled
 
-
+# testdata
 # bloemendaal = 52.3992479,4.630414
-#points = [(34.093523, -118.274893), (34.091414, -118.275887), (34.092082, -118.278062), (34.093867, -118.276609), (34.093523, -118.274893)]
-#polygon = Polygon(points)
-
-#geojson = '{"type": "Polygon", "coordinates": [  [[52, 4.7], [52, 5], [53, 5], [53, 4.7], [52, 4.7]]   ]  }'
-#geojson2 = {"type": "Polygon", "coordinates": [  [[53, 4], [53, 5], [54, 5], [54, 4], [53, 4]]   ]  }
-#geojson3 = {"type": "Polygon", "coordinates": [  [[52, 5], [52, 6], [53, 6], [53, 5], [52, 5]]   ]  }
-#s = shape(json.loads(geojson))
-#s2 = shape(geojson2)
-
-#print(json.dumps(mapping(s)))
-
-# read geofence from parameter file
-params = Params()
-geofence = params.get("GeoFence").decode()
-is_geofence_enabled = params.get("IsGeofenceEnabled").decode() == '1'
-
-print (params.get("IsFcwEnabled").decode())
-print (geofence)
-
-if geofence == '':
-  is_geofence_enabled = False
-
+# points = [(34.093523, -118.274893), (34.091414, -118.275887), (34.092082, -118.278062), (34.093867, -118.276609), (34.093523, -118.274893)]
+# polygon = Polygon(points)
+# geojson = '{"type": "Polygon", "coordinates": [  [[52, 4.7], [52, 5], [53, 5], [53, 4.7], [52, 4.7]]   ]  }'
+# geojson2 = {"type": "Polygon", "coordinates": [  [[53, 4], [53, 5], [54, 5], [54, 4], [53, 4]]   ]  }
+# geojson3 = {"type": "Polygon", "coordinates": [  [[52, 5], [52, 6], [53, 6], [53, 5], [52, 5]]   ]  }
+# s = shape(json.loads(geojson))
+# s2 = shape(geojson2)
+# print(json.dumps(mapping(s)))
 #geofence = {"xxxx/34"}
 #geofence = {"type": "Polygon", "coordinates": [  [[52, 5], [52, 6], [53, 6], [53, 5], [52, 5]]   ]  }
 
-if is_geofence_enabled:
-  try:
-    # convert the string into a dict and create a shape in shapely
-    geofence_dict = json.loads(geofence)
-    geofence_shape = shape(geofence_dict)
-  except AttributeError:
-    is_geofence_enabled = False
-    cloudlog.info('GeoJSON from param file could not be converted into a shape')
-  except JSONDecodeError:
-    is_geofence_enabled = False
-    cloudlog.info('Incorrect GeoJSON found in param file')
+def read_geofence():
+    # read geofence from parameter file nad convert to string, enabled=1 means True
+    params = Params()
+    geofence = params.get("GeoFence").decode()
+    is_geofence_enabled = params.get("IsGeofenceEnabled").decode() == '1'
 
-print (is_geofence_enabled)
+    # print (params.get("IsFcwEnabled").decode())
+    # print (geofence)
 
+    if geofence == '':
+        is_geofence_enabled = False
+
+    if is_geofence_enabled:
+        try:
+        # convert the string into a dict and create a shape in shapely
+        geofence_dict = json.loads(geofence)
+        geofence_shape = shape(geofence_dict)
+    except AttributeError:
+        is_geofence_enabled = False
+        cloudlog.info('GeoJSON from param file could not be converted into a shape')
+    except JSONDecodeError:
+        is_geofence_enabled = False
+        cloudlog.info('Incorrect GeoJSON found in param file')
+
+    return is_geofence_enabled, geofence_shape
+        
+
+# main loop
+is_geofence_enabled, geofence_shape = read_geofence()
 while is_geofence_enabled:
     # get gps position from zmq
     lat = 52.3992479
