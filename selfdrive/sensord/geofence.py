1ad24e86e5b0b8e8e06d1f778b77443ff9e46d82
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-07 09:01:01 +0100

    Update geofence.py

--- selfdrive/sensord/geofence.py
+++ selfdrive/sensord/geofence.py
@@ -154,7 +154,7 @@ while True:
         # type= 1
         # messageId = 1
         # data = geofence + result
-        nav_update_segments[0].from = 1
+        nav_update_segments[0].distance = 1
         msg.navUpdate.segments[0].from = 1
         msg.navUpdate.isNavigating = True
         #msg.navUpdate.to = 
