fa92c4a2cc7eb3aaf2e84122c5e7a4e31422e54b
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-07 19:02:11 +0100

    Update pandad.py

--- selfdrive/pandad.py
+++ selfdrive/pandad.py
@@ -2,16 +2,25 @@
 # simple boardd wrapper that updates the panda first
 import os
 from panda import ensure_st_up_to_date
+from common.params import Params
+
+params = Params()
+
+is_panda_absent = params.get("IsPandaAbsent").decode() == '1'
 
 def main(gctx=None):
-  ensure_st_up_to_date()
+
+  if is_panda_absent:
+    print ("No Panda available")
+  else:
+    ensure_st_up_to_date()
   
   print ("Launch boardd")
 
   #os.chdir("boardd")
   #os.execvp("./boardd", ["./boardd"])
   
-  # now done in manager, this is nnot the right place
+  # now done in manager, this is not the right place# but how to make it is launched after the update ??
 
 if __name__ == "__main__":
   main()
