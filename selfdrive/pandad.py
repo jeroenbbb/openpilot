72a8210ab101fe38bf34c3a59fed04f42032a466
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-07 18:35:53 +0100

    Update pandad.py

--- selfdrive/pandad.py
+++ selfdrive/pandad.py
@@ -5,9 +5,13 @@ from panda import ensure_st_up_to_date
 
 def main(gctx=None):
   ensure_st_up_to_date()
+  
+  print ("Launch boardd")
 
-  os.chdir("boardd")
-  os.execvp("./boardd", ["./boardd"])
+  #os.chdir("boardd")
+  #os.execvp("./boardd", ["./boardd"])
+  
+  # now done in manager, this is nnot the right place
 
 if __name__ == "__main__":
   main()
