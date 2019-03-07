51c1cf4b6f028ca6f7ed1af4371ede6a94be4d78
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-07 18:37:27 +0100

    Update manager.py

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -376,8 +376,8 @@ def manager_thread():
         kill_managed_process(p)
 
     # check the status of all processes, did any of them die?
-    pandad_runing = false
-    board_running = false
+    pandad_runing = False
+    board_running = False
     
     for p in running:
       cloudlog.debug("   Running %s %s" % (p, running[p]))
