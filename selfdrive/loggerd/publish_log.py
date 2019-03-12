524715fdf9508f005846467d4ee5247c2428e331
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-12 15:07:48 +0100

    Update publish_log.py

--- selfdrive/loggerd/publish_log.py
+++ selfdrive/loggerd/publish_log.py
@@ -50,7 +50,9 @@ def main(gctx=None):
             #print (mode)
             msg = sock.recv()
             #msg = sock.recv_multipart()
-            print (msg.decode("unicode"))
+            print (str(msg))
+            print (msg.decode("ascii"))
+
         # find the correct socket
         #if socket in socks and socks[socket] == zmq.POLLIN:
         #    msg = socket.recv_multipart()
