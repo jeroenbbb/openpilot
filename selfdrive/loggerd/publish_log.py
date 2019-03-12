78a92715bdac1c93f529ac2388b358788b3305ca
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-12 11:50:03 +0100

    Update publish_log.py

--- selfdrive/loggerd/publish_log.py
+++ selfdrive/loggerd/publish_log.py
@@ -33,7 +33,7 @@ def main(gctx=None):
 
     # loop through all services to listen to the socks
     poller = zmq.Poller()
-    poller.register( service_socket[1],  zmq.POLLIN )
+    poller.register( service_sock[1],  zmq.POLLIN )
     
     while True:
         count = 0
