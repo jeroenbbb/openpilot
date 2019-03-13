68125910d54740f41522678216c6ddfb002259c2
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-13 21:48:03 +0100

    Update test.py

--- selfdrive/loggerd/test.py
+++ selfdrive/loggerd/test.py
@@ -16,6 +16,7 @@ r = requests.get(url = URL, params = PARAMS)
 # extracting data in json format 
 data = r.json() 
   
+print (str(data))  
   
 # extracting latitude, longitude and formatted address  
 # of the first matching location 
