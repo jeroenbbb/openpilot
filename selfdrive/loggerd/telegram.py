da1dd667c3c7969c1df7dfdd33d240c2a175b15e
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-26 11:25:53 +0100

    Update telegram.py

--- selfdrive/loggerd/telegram.py
+++ selfdrive/loggerd/telegram.py
@@ -37,8 +37,8 @@ def get_json_from_url(url):
 
 def get_me():
     url = URL + "getme"
-    url = "https://api.telegram.org/bot" + token + "/getme"
-    print (url)
+    // url = "https://api.telegram.org/bot" + token + "/getme"
+    // print (url)
     js = get_json_from_url(url)
     return js
 
@@ -48,6 +48,8 @@ def get_updates(offset=None):
     if offset:
         url += "?offset={}".format(offset)
     js = get_json_from_url(url)
+    print ("JS=")
+    print (js)
     return js
 
 
