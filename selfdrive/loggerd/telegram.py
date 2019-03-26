675fc040c002370dbbbececd64cd862dea0f1cc5
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-03-26 11:43:04 +0100

    Update telegram.py

--- selfdrive/loggerd/telegram.py
+++ selfdrive/loggerd/telegram.py
@@ -37,8 +37,6 @@ def get_json_from_url(url):
 
 def get_me():
     url = URL + "getme"
-    # url = "https://api.telegram.org/bot" + token + "/getme"
-    # print (url)
     js = get_json_from_url(url)
     return js
 
@@ -48,8 +46,6 @@ def get_updates(offset=None):
     if offset:
         url += "?offset={}".format(offset)
     js = get_json_from_url(url)
-    print ("JS=")
-    print (js)
     return js
 
 
@@ -63,8 +59,9 @@ def get_last_update_id(updates):
 def echo_all(updates):
     for update in updates["result"]:
         text = update["message"]["text"]
-        chat = update["message"]["chat"]["id"]
-        send_message(text, chat)
+        chatId = update["message"]["chat"]["id"]
+        answer = generate_answer(text, chatId)
+        send_message(answer, chatId)
 
 
 def get_last_chat_id_and_text(updates):
@@ -80,7 +77,16 @@ def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
+def generate_answer(text, chat):
+    answer = "Sorry, begrijp ik niet"
+    if text == "/start":
+        answer = "Welkom bij de ScoozyBot. Met deze bot kun je je Scoozy monitoren en opdrachten geven."
 
+    if text.find("waar") > -1:
+        answer = "Ik ben nu in de Platananlaan"
+    
+    return answer
+    
 def main():
     last_update_id = None
     print (get_me())
