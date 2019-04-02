cc2b58b537134c1b0b051065fe7e6e989e391cb1
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-04-02 09:03:04 +0200

    Update publish_log.py

--- selfdrive/loggerd/publish_log.py
+++ selfdrive/loggerd/publish_log.py
@@ -138,7 +138,7 @@ def main(gctx=None):
             print(evt.which())
             
             # remember last message for every message type
-            last_message[evt.which()] = evt.decode('utf-8')
+            last_message[evt.which()] = print(evt)
             
             # check if the message has to be uploaded or not 
             upload_required, field1, field2 = define_upload_required(evt)
