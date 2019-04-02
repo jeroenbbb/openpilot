6dd57d23e117d337d602ea2c468f3821f050f71b
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-04-02 16:59:50 +0200

    Update publish_log.py

--- selfdrive/loggerd/publish_log.py
+++ selfdrive/loggerd/publish_log.py
@@ -93,7 +93,7 @@ def convert_message(evt):
     
     if which == "navUpdate":
         hlp = evt.navUpdate.segments
-        output = hlp[0].instruction.value()
+        output = hlp[0].instruction.returnOutsideGeofence
         print ("**************")
         print (output)
 
