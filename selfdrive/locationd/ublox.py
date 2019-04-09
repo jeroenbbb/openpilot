3d3dc77a00c2ac3a989d8337c66a9d7adbbe2793
jeroenbbb <34515654+jeroenbbb@users.noreply.github.com>
2019-04-09 15:58:23 +0200

    Update ublox.py

--- selfdrive/locationd/ublox.py
+++ selfdrive/locationd/ublox.py
@@ -658,6 +658,8 @@ class UBloxMessage:
     ck_a = 0
     ck_b = 0
     for i in data:
+      print (ck_a, ord(i))
+      
       ck_a = (ck_a + ord(i)) & 0xFF
       ck_b = (ck_b + ck_a) & 0xFF
     return (ck_a, ck_b)
